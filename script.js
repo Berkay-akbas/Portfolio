4d73423e36b93ab177cb248b7e500d1b9a935c0e
Berkay Akbas <berkayakbas@gmail.com>
2022-06-13 18:46:45 +0300

    Fix Lintern

--- script.js
+++ script.js
@@ -1,17 +1,14 @@
-const menu = document.querySelector("#menu");
-const navbar = document.querySelector(".navbar");
-const logo = document.querySelector(".logo");
-
+const menu = document.querySelector('#menu');
+const navbar = document.querySelector('.navbar');
+const logo = document.querySelector('.logo');
 
 menu.addEventListener('click', () => {
-  
   logo.classList.toggle('off');
   navbar.classList.toggle('active');
   menu.classList.toggle('fa-times');
 });
 
 navbar.addEventListener('click', () => {
-  
   logo.classList.remove('off');
   navbar.classList.remove('active');
   menu.classList.remove('fa-times');
