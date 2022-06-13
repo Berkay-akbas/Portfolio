b0043404a907d06e62c54b0270b57ac493c400a4
Berkay Akbas <berkayakbas@gmail.com>
2022-06-13 17:28:50 +0300

    Add Nav Menu

---
+++ script.js
@@ -0,0 +1,23 @@
+const menu = document.querySelector("#menu");
+const navbar = document.querySelector(".navbar");
+const logo = document.querySelector(".logo");
+
+
+menu.addEventListener('click', () => {
+  
+  logo.classList.toggle('off');
+  navbar.classList.toggle('active');
+  menu.classList.toggle('fa-times');
+});
+
+navbar.addEventListener('click', () => {
+  
+  logo.classList.remove('off');
+  navbar.classList.remove('active');
+  menu.classList.remove('fa-times');
+});
+// window.onscroll = () => {
+//   menu.classList.remove('fa-times');
+//   logo.classList.remove('off');
+//   navbar.classList.remove('active');
+// };
